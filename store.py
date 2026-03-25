1ed7bbcfdcb7b6d655c32c4219814b1091283190
Frank Stremmler <Frank.Stremmler@gmx.de>
2026-03-25 10:44:02 +0100

    Pt.2

--- store.py
+++ store.py
@@ -3,9 +3,20 @@ Description for the File
 '''
 
 class Store():
-    ''''''
+    '''
+    Represents a store with a name and inventory management.
+    '''
     name: str
+
     def __init__(self, name: str):
+        self.name = name
+
+    def get_name(self) -> str:
+        '''Return the store name.'''
+        return self.name
+
+    def set_name(self, name: str) -> None:
+        '''Set the store name.'''
         self.name = name
 
 
